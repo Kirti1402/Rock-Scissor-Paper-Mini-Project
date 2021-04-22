7c3b5e8448600a925f224ebaa44e19143f4995d6
Kirti1402 <65915246+Kirti1402@users.noreply.github.com>
2021-04-22 12:27:56 +0530

    Add files via upload

---
+++ Rock_Paper_Scissor.py
@@ -0,0 +1,27 @@
+#Project Rock,Paper and scissor
+import random
+print("Let Start")
+
+user_choice=input("Enter Your option rock 'r',paper 'p',Scissor 's', Exit 'e': ")
+
+actions=['r','p','s']
+comp_choice=random.choice(actions)
+print(f"User choose {user_choice} and Computer choose {comp_choice}")
+
+if user_choice == comp_choice:
+    print("Tie")
+elif user_choice=='r':
+    if comp_choice=='s':
+        print("You win")
+    else:
+        print("you lose")
+elif user_choice=='s':
+    if comp_choice=='p':
+        print("you win")
+    else:
+        print("you lose")
+elif user_choice=='p':
+    if comp_choice=='r':
+        print("you win")
+    else:
+        print("you lose")
